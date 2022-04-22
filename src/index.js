70d9e4c2ba85bb1486dcc0584343ca322aa68600
stolomeo <tolomeosebastian@gmail.com>
2022-04-22 01:18:06 -0400

    Imported searchCity

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import displayData from "./modules/displayData";
-
+import searchCity from "./modules/searchCity";
+import "./styles.css";
 displayData(
   "https://api.openweathermap.org/data/2.5/weather?q=london&APPID=3b6208a31f3ddbc9ccd1d6206a490def&units=imperial"
 );
